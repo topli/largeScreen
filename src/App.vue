0052bb0f1dbf83daa3288efb8bf92cee54587412
topli <tli@incarcloud.com>
2023-07-21 10:07:02 +0800

    公用样式

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,13 @@ import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <style lang="scss">
+@import './styles/common/index.scss';
+@import './styles/layout/app.scss';
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
